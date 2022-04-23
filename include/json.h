a1ac7985950008684a774c28275d8ce52bebeaa6
Weydans Barros <weydans@hotmail.com>
2022-04-23 09:19:48 -0300

    Creating central point to declare structures data types

--- include/json.h
+++ include/json.h
@@ -1,21 +1,20 @@
 #ifndef _JSON_H_
 #define _JSON_H_
 
-typedef struct JSON_MAP JSON_MAP;
+typedef struct JSON_LIST JSON_LIST;
+typedef struct JSON_OBJECT JSON_OBJECT;
+typedef struct JSON_DATA JSON_DATA;
+typedef enum json_data_type json_data_type; 
 
-// ============================================
-// JSON_MAP
-JSON_MAP* json_map_new ();
-void json_map_destroy ( JSON_MAP** map );
-
-void json_map_add_int( JSON_MAP* map, char* key, long long int data );
-void json_map_add_char ( JSON_MAP* map, char* key, char data );
-void json_map_add_list( JSON_MAP* map, char* key, JSON_DATA* data );
-void json_map_dump( JSON_MAP* map );
-char* json_map_to_string_beautify ( JSON_MAP* data, char* tabe, size_t context );
-
-// ============================================
-// JSON_DATA
+enum json_data_type {
+	json_data_type_integer,
+	json_data_type_char,
+	json_data_type_str,
+	json_data_type_bool,
+	json_data_type_double,
+	json_data_type_list,
+	json_data_type_object
+};
 
 #endif
 
