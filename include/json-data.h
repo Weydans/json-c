35a4666fdf9e5e3a8228a45a074ac1b2c529a7de
Weydans Barros <weydans@hotmail.com>
2022-04-23 09:24:48 -0300

    Removing declaration/definition of data types and creating new interfaces: json_data_add_list and json_data_get_list

--- include/json-data.h
+++ include/json-data.h
@@ -2,19 +2,7 @@
 #define _JSON_DATA_H_
 
 #include <stdbool.h>
-
-typedef struct JSON_DATA JSON_DATA;
-typedef enum json_data_type json_data_type; 
-
-enum json_data_type {
-	json_data_type_integer,
-	json_data_type_char,
-	json_data_type_str,
-	json_data_type_bool,
-	json_data_type_double,
-	json_data_type_list,
-	json_data_type_object
-};
+#include "json.h"
 
 JSON_DATA* json_data_new ();
 void json_data_add_str ( JSON_DATA* data, char* value );
@@ -22,8 +10,10 @@ void json_data_add_bool ( JSON_DATA* data, bool value );
 void json_data_add_char ( JSON_DATA* data, char value );
 void json_data_add_integer ( JSON_DATA* data, long long int value );
 void json_data_add_double ( JSON_DATA* data, long double data_double );
+void json_data_add_list ( JSON_DATA* data, JSON_LIST* list );
 char* json_data_to_string ( JSON_DATA* data );
 json_data_type json_data_get_type ( JSON_DATA* data );
+JSON_LIST* json_data_get_list ( JSON_DATA* data );
 void json_data_destroy ( JSON_DATA** data );
 
 #endif
