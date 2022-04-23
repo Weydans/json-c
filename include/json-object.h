4631d0826739f82814e49b2c139a509e15af4156
Weydans Barros <weydans@hotmail.com>
2022-04-23 09:33:48 -0300

    Removing declaration of data taype and adding json_object_add_list interface

--- include/json-object.h
+++ include/json-object.h
@@ -1,14 +1,13 @@
 #ifndef _JSON_OBJECT_H_
 #define _JSON_OBJECT_H_
 
-typedef struct JSON_OBJECT JSON_OBJECT;
-
 JSON_OBJECT* json_object_new ();
 void json_object_add_str ( JSON_OBJECT* obj, char* key, char* value );
 void json_object_add_char ( JSON_OBJECT* obj, char* key, char value );
 void json_object_add_bool ( JSON_OBJECT* obj, char* key, bool value );
 void json_object_add_integer ( JSON_OBJECT* obj, char* key, long long int value );
 void json_object_add_double ( JSON_OBJECT* obj, char* key, long double value );
+void json_object_add_list ( JSON_OBJECT* obj, char* key, JSON_LIST* list );
 char* json_object_to_string ( JSON_OBJECT* obj );
 char* json_object_to_string_beautify ( JSON_OBJECT* obj, char* tabe, size_t context );
 void json_object_destroy ( JSON_OBJECT** obj );
